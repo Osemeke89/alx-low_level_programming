a6ab81e72ed053055c01a845fe759901d8ec8ccf
Osemeke Onyekachukwu <52927146+Osemeke89@users.noreply.github.com>
2022-10-28 11:18:27 +0100

    commit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: number to iterate over
+ * @index: index to clear at
+ * Return: 1 if worked or -1 if there was an error
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int bit;
+
+	if (index > 31)
+		return (-1);
+	bit = ~(1 << index);
+
+	*n = *n & bit;
+
+	return (1);
+}
