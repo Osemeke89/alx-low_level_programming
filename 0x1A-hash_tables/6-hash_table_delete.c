d73838b00497ec61471d695598a64c6849c60c91
Osemeke Onyekachukwu <52927146+Osemeke89@users.noreply.github.com>
2023-01-10 04:23:55 -0800

    commit

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,38 +1,35 @@
+/*
+ * File: 6-hash_table_delete.c
+ * Auth: Brennan D Baraban
+*/
+
 #include "hash_tables.h"
-#include <stdlib.h>
 
 /**
- * hash_table_delete - frees and empties a hash table
- * @ht: hash table to delete
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
  */
 void hash_table_delete(hash_table_t *ht)
 {
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
 	unsigned long int i;
-	hash_node_t *next_node = NULL, *node = NULL;
 
-	if (!ht)
-		return;
-
-	if (ht->array)
+	for (i = 0; i < ht->size; i++)
 	{
-		for (i = 0; i < ht->size; i++)
+		if (ht->array[i] != NULL)
 		{
-			if (ht->array[i] != NULL)
+			node = ht->array[i];
+			while (node != NULL)
 			{
-				node = ht->array[i];
-				while (node)
-				{	
-					next_node = node->next;
-					if (node->value)
-						free(node->value);
-					if (node->key)
-						free(node->key);
-					free(node);
-					node = next_node;
-				}
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
 			}
 		}
-		free(ht->array);
 	}
-	free(ht);
+	free(head->array);
+	free(head);
 }
